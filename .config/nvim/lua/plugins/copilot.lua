4d1dff03ca67f248854ef69fa132a3bbca9bbb33
Fabio Nukui <fabio@nukui@gmail.com>
2024-02-05 18:42:20 +0000

    replace some copilot keymaps

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,4 +1,8 @@
 -- Github copilot integration
 return {
     "github/copilot.vim",
+    config = function()
+        vim.keymap.set('i', '<C-l>', '<Plug>(copilot-accept-word)')
+        vim.keymap.set('i', '<C-j>', '<Plug>(copilot-accept-line)')
+    end,
 }
